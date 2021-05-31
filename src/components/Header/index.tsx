a4828ba3e20daf48dea0bc9108d259403bc11093
mb4ndeira <mbandeira.cs@gmail.com>
2021-05-30 22:11:23 -0300

    fix header logo containment

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import styles from "./header.module.scss"
 
 export default function Header() {
     return (
-        <header>
+        <header className={styles.header}>
             <a className={styles.logo} href="/">
                 <img src="/images/logo.png" alt="" />
                 <h2>twitter<span>_observer</span></h2>
